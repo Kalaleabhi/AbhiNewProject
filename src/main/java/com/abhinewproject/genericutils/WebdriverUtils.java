22b3826ebb250347b51b40dd1e7019bdac324156
kalaleabhi <abhishekkalale19@gmail.com>
2023-09-28 16:28:21 +0530

    1 st commit

---
+++ src/main/java/com/abhinewproject/genericutils/WebdriverUtils.java
@@ -0,0 +1,12 @@
+package com.abhinewproject.genericutils;
+
+import org.testng.annotations.Test;
+
+public class WebdriverUtils {
+
+	@Test
+	public void maximizeBrowser()
+	{
+		System.out.println(" from maximize browser");
+	}
+}
---
+++ src/test/java/com/abhinewproject/testscripts/SampleTest.java
@@ -0,0 +1,12 @@
+package com.abhinewproject.testscripts;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void demoTest()
+	{
+		System.out.println(" from sample test ");
+	}
+}
