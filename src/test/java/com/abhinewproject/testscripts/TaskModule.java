046739027ed4fe8b0366708accb2054284394ad0
kalaleabhi <abhishekkalale19@gmail.com>
2023-09-28 16:30:32 +0530

    admin 1 st commit

---
+++ src/test/java/com/abhinewproject/testscripts/TaskModule.java
@@ -0,0 +1,12 @@
+package com.abhinewproject.testscripts;
+
+import org.testng.annotations.Test;
+
+public class TaskModule {
+
+	@Test
+	public void taskModuleTest()
+	{
+		System.out.println(" From task module");
+	}
+}
